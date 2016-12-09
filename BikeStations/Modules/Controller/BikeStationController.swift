6f7c2442b194daaf8084b4d81b23e1d0777d00b2
Nino <scollaco@gmail.com>
2016-12-08 22:06:28 -0500

    Replaced BikeStation class for struct

--- BikeStations/Modules/Controller/BikeStationController.swift
+++ BikeStations/Modules/Controller/BikeStationController.swift
@@ -10,8 +10,9 @@ import UIKit
 import Alamofire
 
 protocol BikeStationClientProtocol {
-
+    
     func getBikeStations(completion : @escaping  BikeStationsCompletion)
+    func getBikeStationInfo(stationId: String, completion : @escaping UniqueStationCompletion)
 }
 
 typealias BikeStationsCompletion = ([BikeStation]?, String?) -> Void
@@ -21,14 +22,16 @@ typealias UniqueStationCompletion = (BikeStation?, String?) -> Void
 class BikeStationController: NSObject, BikeStationClientProtocol {
     
     var bikeStations : [BikeStation]! = []
-    let bikeStationsEndPoint = "gbfs/en/station_information.json"
-    let uniqueStationEndPoint = "gbfs/en/station_status.json"
-    
     
     func getBikeStations(completion : @escaping BikeStationsCompletion) {
         
-        let urlString  = BASE_URL + bikeStationsEndPoint
-        let url = URL.init(string: urlString)
+        let url = urlWith(string: "gbfs/en/station_information.json")
+        
+        guard url != nil else {
+        
+            completion(nil, "")
+            return
+        }
         
         Alamofire.request(url!, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil).responseJSON { (response:DataResponse<Any>) in
             
@@ -53,39 +56,31 @@ class BikeStationController: NSObject, BikeStationClientProtocol {
     
     func getBikeStationInfo(stationId: String, completion : @escaping UniqueStationCompletion) {
         
-        let urlString  = BASE_URL + uniqueStationEndPoint
-        let url = URL.init(string: urlString)
+        let url = urlWith(string: "gbfs/en/station_status.json")
+        
+        guard url != nil else {
+            
+            completion(nil, "")
+            return
+        }
         
         Alamofire.request(url!, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil).responseJSON { (response:DataResponse<Any>) in
             
-              completion(nil, response.result.error.debugDescription)
-            
-           // guard let json = response.result.value as? [String:AnyObject] else {
-             //   completion(nil, response.result.error.debugDescription)
-             //   return
-           // }
-            
-
-           // let stations : [Any]? = json["data"]?["stations"] as? [[String : Any]]
-           // var station : BikeStationInfo?
-            
-            //stations?.forEach {(dictionary) in
-            
-              //  if let id =  dictionary["station_id"] as String {
-                
-                //    if id == station
-                
-                
-              //  }
-                
-            //}
-            
-            //let station = stations?.filter{ ($0 as! Dictionary)["station_id"] == stationId }
-       
-            //print(station)
-            
-            //completion (self.bikeStations, nil)
+            completion(nil, response.result.error.debugDescription)
         }
     }
-    
 }
+
+extension BikeStationController {
+
+    func urlWith(string : String) -> URL? {
+    
+        let urlString  = BASE_URL + string
+        if let url = URL.init(string: urlString) {
+            return url
+        } else {
+            return nil
+        }
+    }
+}
+
--- BikeStations/Modules/Model/BikeStation.swift
+++ BikeStations/Modules/Model/BikeStation.swift
@@ -17,37 +17,30 @@ protocol Annotable {
     var longitude : Double? { get set }
 }
 
-final class BikeStation : NSObject, Annotable {
+struct BikeStation {
     
     var stationId : String?
     var name : String?
     var latitude : Double?
     var longitude : Double?
     let store = BikeStationStore()
- 
     
     var coordinate : CLLocationCoordinate2D  {
         get {
            return CLLocationCoordinate2D(latitude: latitude!, longitude: longitude!)
         }
     }
-    
+}
+
+extension BikeStation {
+
     init(dictionary : [String : Any]) {
         
-         self.name = dictionary["name"] as? String
-         self.stationId = dictionary["station_id"] as? String
-         self.latitude =  dictionary["lat"] as? Double
-         self.longitude =  dictionary["lon"] as? Double
+        self.name = dictionary["name"] as? String
+        self.stationId = dictionary["station_id"] as? String
+        self.latitude =  dictionary["lat"] as? Double
+        self.longitude =  dictionary["lon"] as? Double
     }
-    
-    override var description: String {
-    
-        return "Name : \(name)" +
-               "\nStation ID : \(stationId)" +
-               "\nLatitude : \(latitude)" +
-               "\nLongitude : \(longitude)"
-    }
-    
 }
 
 
